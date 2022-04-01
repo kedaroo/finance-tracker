d5bf62f0253ae10196541a12fccc25560462c9e5
kedaroo <krb1202@gmail.com>
2022-04-01 22:52:48 +0530

    feat: uid in useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { projectFirestore } from '../firebase/config'
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
+    const query = useRef(_query).current
 
     useEffect(() => {
         let ref = projectFirestore.collection(collection)
+
+        if (query) {
+            ref = ref.where(...query)
+        }
 
         const unsubscribe = ref.onSnapshot(snapshot => {
             let results = []
@@ -23,7 +28,7 @@ export const useCollection = (collection) => {
 
         return () => unsubscribe()
 
-    }, [collection])
+    }, [collection, query])
 
     return { documents, error }
 }
